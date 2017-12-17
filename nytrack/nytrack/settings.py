47385d9a37854b5c35439918ea7de33ac96caf99
Haris <harisnadeem2008@hotmail.com>
2017-12-16 20:21:58 -0500

    Created ER diagram and revised database model

--- nytrack/nytrack/settings.py
+++ nytrack/nytrack/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'nytrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     'default' : {
-#         'ENGINE' : 'django.db.backends.mysql',
-#         'HOST': 'localhost',
-#         'PORT' : '',
-#         'NAME' : 'NYTRACK',
-#         'USER' : 'root',
-#         'PASSWORD' : 'googoodolls'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT' : '',
+        'NAME' : 'NYTRACK',
+        'USER' : 'root',
+        'PASSWORD' : 'googoodolls'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
